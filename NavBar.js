45455dd7035fbf0d88ce4c9f22cc8f3da8969cdb
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-22 19:00:19 -0400

    Create NavBar

---
+++ NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/articles-list">Article List</Link>
+            </li>
+            <li>
+                <Link to="/article-page">Article Page</Link>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path='/' component={HomePage}/>
+        <div id="page-body">
+          <Route path='/' component={HomePage} exact/>
+          <Route path='/about' component={AboutPage}/>
+          <Route path='/articles-list' component={ArticlesList}/>
+          <Route path='/article-page' component={ArticlePage}/>
+        </div>
       </div>
     </Router>
   );
